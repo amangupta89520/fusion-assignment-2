8d8befa628d05cf2a8f8eadeb4c8e8045872ee25
Aman Gupta <aman.gupta@commercepundit.com>
2024-05-23 23:05:52 +0530

    Initial Setup - Added Shadcn UI, tailwind CSS, typescirpt setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { cn } from "./lib/utils"
+
+function App() {
+
+  return (
+    <div>
+      <h1 className={cn('text-9xl')}>This is basic setup for fusion assignment 2</h1>
+    </div>
+  )
+}
+
+export default App
